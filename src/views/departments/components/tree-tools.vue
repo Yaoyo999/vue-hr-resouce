ae0f25b9859b72ca195fc571660f3bf10231e173
Yaoyo999 <964862992@qq.com>
2021-05-10 20:28:29 +0800

    将树形内容单独抽提组件

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,52 @@
+<template>
+    <el-row slot-scope="{treeNode}" type="flex" justify="space-between" align="middle" style="height:40px;fontSize:14px;width:100%;">
+          <el-col>
+            <span>{{treeNode.name}}</span>
+          </el-col>
+          <el-col :span="4">
+            <el-row justify="end" type="flex">
+              <el-col>{{treeNode.manager}}</el-col>
+              <el-col>
+                <el-dropdown>
+                  <span class="el-dropdown-link">
+                    操作<i class="el-icon-arrow-down el-icon--right"></i>
+                  </span>
+                  <el-dropdown-menu slot="dropdown">
+                      <el-dropdown-item>添加子部门</el-dropdown-item>
+                      <el-dropdown-item v-if="!isRoot">编辑部门</el-dropdown-item>
+                      <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </el-col>
+        </el-row>
+</template>
+
+<script>
+export default {
+  name: 'treeTools',
+  components: {},
+  props: {
+    treeNode: {
+      isRequired: true,
+      type: Object
+    },
+    // 判断是不是根节点是不需要删除部门和编辑部门的（组件定制）
+    isRoot: {
+      default: false,
+      type: Boolean
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -3,70 +3,23 @@
     <div class="app-container">
       <el-card class="box-card tree-card">
         <!--头部结构 -->
-        <el-row type="flex" justify="space-between" align="middle" style="height:40px;fontSize:14px;">
-          <el-col>
-            <span>江苏传智播客教育科技股份有限公司</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row justify="end" type="flex">
-              <el-col>负责人</el-col>
-              <el-col>
-                <el-dropdown>
-                  <span class="el-dropdown-link">
-                    操作<i class="el-icon-arrow-down el-icon--right"></i>
-                  </span>
-                  <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>添加子部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+        <tree-tools :treeNode="company" :isRoot="true"></tree-tools>
         <!-- 树形内容 -->
         <el-tree :data="departs" :props="defaultProps" @node-click="handleNodeClick">
-           <el-row slot-scope="{data}" type="flex" justify="space-between" align="middle" style="height:40px;fontSize:14px;width:100%;">
-          <el-col>
-            <span>{{data.name}}</span>
-          </el-col>
-          <el-col :span="4">
-            <el-row justify="end" type="flex">
-              <el-col>{{data.manager}}</el-col>
-              <el-col>
-                <el-dropdown>
-                  <span class="el-dropdown-link">
-                    操作<i class="el-icon-arrow-down el-icon--right"></i>
-                  </span>
-                  <el-dropdown-menu slot="dropdown">
-                      <el-dropdown-item>添加子部门</el-dropdown-item>
-                      <el-dropdown-item>编辑部门</el-dropdown-item>
-                      <el-dropdown-item>删除部门</el-dropdown-item>
-                  </el-dropdown-menu>
-                </el-dropdown>
-              </el-col>
-            </el-row>
-          </el-col>
-        </el-row>
+         <tree-tools slot-scope="{ data }" :treeNode="data"></tree-tools>
         </el-tree>
-      </el-card>
+      </el-card> 
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
-
-<script>
+import TreeTools from './components/tree-tools'
 export default {
   name: '',
-  components: {},
+  components: {
+    TreeTools
+  },
   props: {},
   data () {
     return {
@@ -77,6 +30,7 @@ export default {
       departs: [{ name: '总裁办', manager: '曹操', children: [{ name: '董事会', manager: '曹丕' }] },
         { name: '行政部', manager: '刘备' },
         { name: '人事部', manager: '孙权' }],
+      company: { name: '江苏传智播客教育科技股份有限公司', manager: '负责人' },
     }
   },
   computed: {},
